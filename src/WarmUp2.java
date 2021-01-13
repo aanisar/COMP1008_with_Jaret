61aacdc705a4ba7513f344fe84f060e8540f5071
= <=>
2021-01-13 13:54:54 -0500

    hi

--- src/WarmUp2.java
+++ src/WarmUp2.java
@@ -1,9 +1,16 @@
 public class WarmUp2 {
     public static void main(String[] args) {
 
+        System.out.println("A string"); // this creates a new line after the String is printed
         System.out.printf("timeToSki(30) should be false: %b%n", timeToSki(30));
-        System.out.printf("timeToSki(31) should be false: %b%n", timeToSki(31));
+        System.out.printf("timeToSki(31) should be true: %b%n", timeToSki(31));
 
+        double gpa = 98.76567854567;
+        System.out.println("println gpa =" +gpa);
+        System.out.printf("printf gpa =%.1f%n",gpa);
+
+        for (int i=0; i<=10000; i+=100)
+            System.out.printf("%d%n",i);
     }
     public static boolean timeToSki (int snowDepth)
     {
