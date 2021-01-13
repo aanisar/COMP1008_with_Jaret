77e206cb959f533c7e07e122d24bb27d57efa4bf
= <=>
2021-01-13 13:24:32 -0500

    Initial commit

---
+++ src/WarmUp1.java
@@ -0,0 +1,26 @@
+public class WarmUp1 {
+    public static void main(String[] args)
+    {
+        System.out.println("First Question");
+        //First Question
+        for(int i=100; i>=0; i-=5)
+            System.out.println(i);
+
+        System.out.println("Second Question");
+        //Second Question
+        for(int i =0; i<=100; i+=10)
+            System.out.println(i);
+
+        System.out.println("Third Question");
+        //Third Question
+        for(int i =0; i<=100; i+=10)
+        {
+            if (i%20 ==0)
+                System.out.println(i + " Java");
+            else
+                System.out.println(i);
+        }
+
+
+    }
+}
